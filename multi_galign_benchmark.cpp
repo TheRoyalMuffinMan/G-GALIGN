b112f7cb4acb4b44084cb9b9a4d8d12d09c9afed
Zachary Benson <oakbenson20@gmail.com>
2024-12-08 01:35:13 -0600

    Full Block Creation Working
    Anti Diagonal Segmentation of the matrix works for the full matrix. 3 threads  = 9 total blocks that are created, for 5 anti diagonals

--- multi_galign_benchmark.cpp
+++ multi_galign_benchmark.cpp
@@ -165,7 +165,7 @@ int main(int argc, char *argv[]) {
         {
             // ROW //
             // Calculate a new block moving along the rows (col values stay the same)
-            int newStartRow = existingBlocks[n].endRow;
+            int newStartRow = existingBlocks[n].endRow + 1;
             int newStopRow = existingBlocks[n].endRow + numRowsToAdd;
 
             // Check if this block is out of bounds (error state)
@@ -198,7 +198,7 @@ int main(int argc, char *argv[]) {
                 // Save the Block
                 newBlocks[newBlockIndex].startCol = existingBlocks[n].startCol;
                 newBlocks[newBlockIndex].endCol = existingBlocks[n].endCol;
-                newBlocks[newBlockIndex].startRow = newStartRow + 1;
+                newBlocks[newBlockIndex].startRow = newStartRow;
                 newBlocks[newBlockIndex].endRow = newStopRow;
                 newBlockIndex++;
 
@@ -211,11 +211,11 @@ int main(int argc, char *argv[]) {
 
             // COL //
             // Calculate a new block moving along the cols (row values stay the same)
-            int newStartCol = existingBlocks[n].endCol;
+            int newStartCol = existingBlocks[n].endCol + 1;
             int newStopCol = existingBlocks[n].endCol + numColsToAdd;
 
             // Check if this block is out of bounds (error state)
-            if (newStartRow > queryL || newStopRow > queryL)
+            if (newStartCol > refL || newStopCol > refL)
             {
                 std::cout << "ERROR IN SUB-BLOCK ROW CALCULATION \n";
                 return 0;
@@ -242,7 +242,7 @@ int main(int argc, char *argv[]) {
                 }
 
                 // Save the Block
-                newBlocks[newBlockIndex].startCol = newStartCol + 1;
+                newBlocks[newBlockIndex].startCol = newStartCol;
                 newBlocks[newBlockIndex].endCol = newStopCol;
                 newBlocks[newBlockIndex].startRow = existingBlocks[n].startRow;
                 newBlocks[newBlockIndex].endRow = existingBlocks[n].endRow;
@@ -280,10 +280,111 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Loop over the remaining diagonals, shrinking the number of active threads until we calculate the last sub block
-    while (numThreads > 0)
+    //Loop over the remaining diagonals, shrinking the number of active threads until we calculate the last sub block
+    while (numThreads > -1)
     {
+        // List of new matrix blocks for the next set of threads, & Index
+        threadBlock newBlocks[nTHREADS];
+        int newBlockIndex = 0;
 
+        // For Each Block From the Previous Itteration, calculate the next two blocks
+        for (int n = 0; n < numThreads; n++)
+        {
+            // ROW //
+            // Calculate a new block moving along the rows (col values stay the same)
+            int newStartRow = existingBlocks[n].endRow + 1;
+            int newStopRow = existingBlocks[n].endRow + numRowsToAdd;
+
+            // Check if this block is NOT out of bounds (error state)
+            if (newStopRow <= queryL)
+            {
+                // Check if we have already added this sub-block
+                bool newRowBlock = true;
+                for (int i = 0; i < numThreads + 1; i++)
+                {
+                    if (newBlocks[i].startRow == newStartRow)
+                    {
+                        newRowBlock = false;
+                        break;
+                    }
+                }
+
+                // If it is a new block
+                if (newRowBlock == true)
+                {
+                    // Check if we are near the end of the string, if so bump out the end column
+                    if ((queryL - newStopRow) < (nTHREADS + 1))
+                    {
+                        newStopRow = queryL;
+                    }
+
+                    // Save the Block
+                    newBlocks[newBlockIndex].startCol = existingBlocks[n].startCol;
+                    newBlocks[newBlockIndex].endCol = existingBlocks[n].endCol;
+                    newBlocks[newBlockIndex].startRow = newStartRow;
+                    newBlocks[newBlockIndex].endRow = newStopRow;
+                    newBlockIndex++;
+
+                    // Get the Row, Col, Reference Segment, & Sequence Segment
+
+                    // Create a new Thread
+                    std::cout << "RD : Itteration " << numThreads << "  New Block: " << existingBlocks[n].startCol << " Col End: " << existingBlocks[n].endCol << " Row Start: " << newStartRow << " Row End: " <<  newStopRow << "\n";
+
+                }
+            }
+
+            // COL //
+            // Calculate a new block moving along the cols (row values stay the same)
+            int newStartCol = existingBlocks[n].endCol + 1;
+            int newStopCol = existingBlocks[n].endCol + numColsToAdd;
+
+            // Check if this block is NOT out of bounds (error state)
+            if (newStopCol <= refL)
+            {
+                // Check if we have already added this sub-block
+                bool newColBlock = true;
+                for (int j = 0; j < numThreads + 1; j++)
+                {
+                    if (newBlocks[j].startCol == newStartCol)
+                    {
+                        newColBlock = false;
+                        break;
+                    }
+                }
+
+                // If it is a new block
+                if (newColBlock == true)
+                {
+                    // Check if we are near the end of the string, if so bump out the end column
+                    if ((refL - newStopCol) < (nTHREADS + 1))
+                    {
+                        newStopCol = refL;
+                    }
+
+                    // Save the Block
+                    newBlocks[newBlockIndex].startCol = newStartCol;
+                    newBlocks[newBlockIndex].endCol = newStopCol;
+                    newBlocks[newBlockIndex].startRow = existingBlocks[n].startRow;
+                    newBlocks[newBlockIndex].endRow = existingBlocks[n].endRow;
+                    newBlockIndex++;
+
+                    // Get the Row, Col, Reference Segment, & Sequence Segment
+
+                    // Create a new Thread
+                    std::cout << "CD : Itteration " << numThreads << "  New Block: " << newStartCol << " Col End: " << newStopCol << " Row Start: " << existingBlocks[n].startRow << " Row Stop: " <<  existingBlocks[n].endRow << "\n";
+                }
+            }
+        }
+
+        // Decrease the thread count by 1 
+        numThreads--;
+
+        // Save the current thread blocks
+        for (int z = 0; z < nTHREADS; z++)
+        {
+            existingBlocks[z] = newBlocks[z];
+        }
+      
     }
 
     // End the Timing Calculations
@@ -293,158 +394,158 @@ int main(int argc, char *argv[]) {
               << duration.count() << " seconds" << std::endl;  // Convert seconds to microseconds
 
     
-    //####################################### ANALYZE RESULTS ########################################
-    // Trace the Optimal Path
-    // Init Variables for output
-    int64_t finalScore = 0;
-    std::string refSequence = ""; // Reference Sequence w/gaps
-    std::string alignmentVis = "";
-    std::string queSequence = ""; // Query Sequence w/gaps
+    // //####################################### ANALYZE RESULTS ########################################
+    // // Trace the Optimal Path
+    // // Init Variables for output
+    // int64_t finalScore = 0;
+    // std::string refSequence = ""; // Reference Sequence w/gaps
+    // std::string alignmentVis = "";
+    // std::string queSequence = ""; // Query Sequence w/gaps
 
-    int indexQ = 0;
-    int indexR = 0;
+    // int indexQ = 0;
+    // int indexR = 0;
 
-    // If we are ignoring start gaps, different start location
-    if(ignoreSEGaps)
-    {
-        // Starting Point Initially lower right corner
-        indexQ = rawQuery.length();
-        indexR = rawReference.length();
-        int maxScore = matrixValues[indexQ][indexR];
+    // // If we are ignoring start gaps, different start location
+    // if(ignoreSEGaps)
+    // {
+    //     // Starting Point Initially lower right corner
+    //     indexQ = rawQuery.length();
+    //     indexR = rawReference.length();
+    //     int maxScore = matrixValues[indexQ][indexR];
 
-        // Scan last column and last row for a higher score, if so change start indexes to that tile
-        // Scan each row of the last column
-        for(int q=0; q < rawQuery.length(); q ++)
-        {
-            int newScore = matrixValues[q][rawReference.length()];
-            if (newScore > maxScore)
-            {
-                maxScore = newScore;
-                indexQ = q;
-                indexR = rawReference.length();
-            }
-        }
-        // Scan each col of the last row
-        for (int r=0; r < rawReference.length(); r++)
-        {
-            int newScore = matrixValues[rawQuery.length()][r];
-            if (newScore > maxScore)
-            {
-                maxScore = newScore;
-                indexQ = rawQuery.length();
-                indexR = r;
-            }
-        }
+    //     // Scan last column and last row for a higher score, if so change start indexes to that tile
+    //     // Scan each row of the last column
+    //     for(int q=0; q < rawQuery.length(); q ++)
+    //     {
+    //         int newScore = matrixValues[q][rawReference.length()];
+    //         if (newScore > maxScore)
+    //         {
+    //             maxScore = newScore;
+    //             indexQ = q;
+    //             indexR = rawReference.length();
+    //         }
+    //     }
+    //     // Scan each col of the last row
+    //     for (int r=0; r < rawReference.length(); r++)
+    //     {
+    //         int newScore = matrixValues[rawQuery.length()][r];
+    //         if (newScore > maxScore)
+    //         {
+    //             maxScore = newScore;
+    //             indexQ = rawQuery.length();
+    //             indexR = r;
+    //         }
+    //     }
 
-        // Save the score at the end tile of the path we are traversing (this is the total score of that path)
-        finalScore = matrixValues[indexQ][indexR];
+    //     // Save the score at the end tile of the path we are traversing (this is the total score of that path)
+    //     finalScore = matrixValues[indexQ][indexR];
 
-        // If we dont start in the lower right corner of the matrix we must add in the necessary end gaps to the 3, strings
-        // If we are in the last row but not the last column
-        if (indexQ == rawQuery.length() && indexR != rawReference.length())
-        {
-            int tempR = indexR + 1;
-            //int tempQ = indexQ + 1;
-            // Traverse the matrix back to the lower right corner
-            while (tempR < (rawReference.length() + 1))
-            {
-                // Add gap to query, and visualization string
-                // Add charecters consumed in the reference string
-                alignmentVis = alignmentVis + " ";
-                refSequence = refSequence + rawReference[tempR - 1];
-                queSequence = queSequence + "_";
-                tempR = tempR + 1;
-            }
-        }
-        // If we are in the last column but not the last row
-        else if(indexR == rawReference.length() && indexQ != rawQuery.length())
-        {
-            //int tempR = indexR + 1;
-            int tempQ = indexQ + 1;
-            // Traverse the matrix back to the lower right corner
-            while(tempQ < (rawQuery.length() + 1))
-            {
-                // Add gap to reference, and visualization string
-                // Add charecters consumed in the query string
-                alignmentVis = alignmentVis + " ";
-                queSequence = queSequence + rawQuery[tempQ - 1];
-                refSequence = refSequence + "_";
-                tempQ = tempQ + 1;
-            }
-        }
-    }
-    else
-    {
-        // Starting Point w/start and end gap penalties
-        // Lower right corner of the matrix
-        indexQ = rawQuery.length();
-        indexR = rawReference.length();
+    //     // If we dont start in the lower right corner of the matrix we must add in the necessary end gaps to the 3, strings
+    //     // If we are in the last row but not the last column
+    //     if (indexQ == rawQuery.length() && indexR != rawReference.length())
+    //     {
+    //         int tempR = indexR + 1;
+    //         //int tempQ = indexQ + 1;
+    //         // Traverse the matrix back to the lower right corner
+    //         while (tempR < (rawReference.length() + 1))
+    //         {
+    //             // Add gap to query, and visualization string
+    //             // Add charecters consumed in the reference string
+    //             alignmentVis = alignmentVis + " ";
+    //             refSequence = refSequence + rawReference[tempR - 1];
+    //             queSequence = queSequence + "_";
+    //             tempR = tempR + 1;
+    //         }
+    //     }
+    //     // If we are in the last column but not the last row
+    //     else if(indexR == rawReference.length() && indexQ != rawQuery.length())
+    //     {
+    //         //int tempR = indexR + 1;
+    //         int tempQ = indexQ + 1;
+    //         // Traverse the matrix back to the lower right corner
+    //         while(tempQ < (rawQuery.length() + 1))
+    //         {
+    //             // Add gap to reference, and visualization string
+    //             // Add charecters consumed in the query string
+    //             alignmentVis = alignmentVis + " ";
+    //             queSequence = queSequence + rawQuery[tempQ - 1];
+    //             refSequence = refSequence + "_";
+    //             tempQ = tempQ + 1;
+    //         }
+    //     }
+    // }
+    // else
+    // {
+    //     // Starting Point w/start and end gap penalties
+    //     // Lower right corner of the matrix
+    //     indexQ = rawQuery.length();
+    //     indexR = rawReference.length();
 
-        // Save the score of the path we are going to traverse in reverse
-        finalScore = matrixValues[indexQ][indexR];
-    }
+    //     // Save the score of the path we are going to traverse in reverse
+    //     finalScore = matrixValues[indexQ][indexR];
+    // }
 
-    // Loop until we reach the start of the matrix (0,0)
-    while (indexQ != 0 || indexR !=0)
-    {
-        char direction = matrixDir[indexQ][indexR]; // Get the direction we came from to get to the current tile
-        char symbolM = matrixSym[indexQ][indexR]; // Get the symbol of the current tile (match, gap, or mismatch)
+    // // Loop until we reach the start of the matrix (0,0)
+    // while (indexQ != 0 || indexR !=0)
+    // {
+    //     char direction = matrixDir[indexQ][indexR]; // Get the direction we came from to get to the current tile
+    //     char symbolM = matrixSym[indexQ][indexR]; // Get the symbol of the current tile (match, gap, or mismatch)
 
-        // If we got here by a gap from the left
-        if (direction == 'L')
-        {
-            // Add the consumed reference charecter
-            refSequence = rawReference[indexR-1] + refSequence; // String Concatenation
-            // Add a gap in the query sequence
-            queSequence = "_" + queSequence;
-            // Add a gap in the visualization string
-            alignmentVis = " " + alignmentVis;
+    //     // If we got here by a gap from the left
+    //     if (direction == 'L')
+    //     {
+    //         // Add the consumed reference charecter
+    //         refSequence = rawReference[indexR-1] + refSequence; // String Concatenation
+    //         // Add a gap in the query sequence
+    //         queSequence = "_" + queSequence;
+    //         // Add a gap in the visualization string
+    //         alignmentVis = " " + alignmentVis;
 
-            // Update Index (move to the left)
-            indexR = indexR - 1;
-        }
-        // If we got to this tile from a gap from above
-        else if (direction == 'U')
-        {
-            // Add a gap in the reference sequence
-            refSequence = "_" + refSequence; //# String Concatenation
-            // Add the consumed query sequence
-            queSequence = rawQuery[indexQ-1] + queSequence;
-            // Add a gap in the visualization string
-            alignmentVis = " " + alignmentVis;
+    //         // Update Index (move to the left)
+    //         indexR = indexR - 1;
+    //     }
+    //     // If we got to this tile from a gap from above
+    //     else if (direction == 'U')
+    //     {
+    //         // Add a gap in the reference sequence
+    //         refSequence = "_" + refSequence; //# String Concatenation
+    //         // Add the consumed query sequence
+    //         queSequence = rawQuery[indexQ-1] + queSequence;
+    //         // Add a gap in the visualization string
+    //         alignmentVis = " " + alignmentVis;
 
-            // Update Index (move up)
-            indexQ = indexQ - 1;
-        }
-        // If we got to this tile from the diagonal
-        else if(direction == 'D')
-        {
-            // Add the consumed charecters in both the reference and query to their strings
-            refSequence = rawReference[indexR-1] + refSequence; // String Concatenation
-            queSequence = rawQuery[indexQ-1] + queSequence;
-            // Add the symbol (match, or mismatch) to the alignment visualization string
-            alignmentVis = symbolM + alignmentVis;
+    //         // Update Index (move up)
+    //         indexQ = indexQ - 1;
+    //     }
+    //     // If we got to this tile from the diagonal
+    //     else if(direction == 'D')
+    //     {
+    //         // Add the consumed charecters in both the reference and query to their strings
+    //         refSequence = rawReference[indexR-1] + refSequence; // String Concatenation
+    //         queSequence = rawQuery[indexQ-1] + queSequence;
+    //         // Add the symbol (match, or mismatch) to the alignment visualization string
+    //         alignmentVis = symbolM + alignmentVis;
 
-            // Update Index (move up and left)
-            indexQ = indexQ - 1;
-            indexR = indexR - 1;        
-        }
-    }
+    //         // Update Index (move up and left)
+    //         indexQ = indexQ - 1;
+    //         indexR = indexR - 1;        
+    //     }
+    // }
 
-    //####################################### WRITE OUTPUT ###########################################
-    // Open Output File
-    std::ofstream outputF(args.output);
+    // //####################################### WRITE OUTPUT ###########################################
+    // // Open Output File
+    // std::ofstream outputF(args.output);
 
-    // Write Overall Score & Alignment Visualization to Output File
-    outputF << finalScore << "\n";
-    outputF << reference.id << "\n";
-    outputF << refSequence << "\n";
-    outputF << alignmentVis << "\n";
-    outputF << queSequence << "\n";
-    outputF << query.id << "\n";
+    // // Write Overall Score & Alignment Visualization to Output File
+    // outputF << finalScore << "\n";
+    // outputF << reference.id << "\n";
+    // outputF << refSequence << "\n";
+    // outputF << alignmentVis << "\n";
+    // outputF << queSequence << "\n";
+    // outputF << query.id << "\n";
 
-    // Close Output File
-    outputF.close();
+    // // Close Output File
+    // outputF.close();
 
     // FREE ALLOCATED 2D ARRAYS
     for (int i = 0; i < queryL; i++)
